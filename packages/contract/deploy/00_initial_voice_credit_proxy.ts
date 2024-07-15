354aa585a8980870d67ce71dd14f0b9dd5e7e3bd
Ayodeji <olusanyasamson633@gmail.com>
2024-07-15 20:37:01 +0100

    chore:deployed contracts

--- packages/contract/deploy/00_initial_voice_credit_proxy.ts
+++ packages/contract/deploy/00_initial_voice_credit_proxy.ts
@@ -1,21 +1,30 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { InitialVoiceCreditProxyContractName } from "../constants";
-import { initialVoiceCreditProxy } from "../typechain-types/maci-contracts/contracts";
 
 const DEFAULT_INITIAL_VOICE_CREDITS = 99;
 
-const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployer } = await hre.getNamedAccounts();
+const deployContracts: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const { deployer } = await hre.getNamedAccounts();
 
-    await hre.deployments.deploy(InitialVoiceCreditProxyContractName, {
-        from: deployer,
-        args: [DEFAULT_INITIAL_VOICE_CREDITS],
-        log: true,
-        autoMine: true,
-    });
+  await hre.deployments.deploy(InitialVoiceCreditProxyContractName, {
+    from: deployer,
+    args: [DEFAULT_INITIAL_VOICE_CREDITS],
+    log: true,
+    autoMine: true,
+  });
 
-    const intialVoiceCreditProxy = await hre.ethers.getContract(InitialVoiceCreditProxyContractName, deployer);
-    console.log(`The intial voice credit proxy is deployed at ${initialVoiceCreditProxy.getAddress()}`)
+  const initialVoiceCreditProxy = await hre.ethers.getContract(
+    InitialVoiceCreditProxyContractName,
+    deployer
+  );
+  console.log(
+    `The initial voice credit proxy is deployed at ${await initialVoiceCreditProxy.getAddress()}`
+  );
+};
 
-}
+export default deployContracts;
+
+deployContracts.tags = ["InitialVoiceCreditProxy"];
